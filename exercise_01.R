c6107598aa32cf5150d9e4a79b9e1b361e74391d
Bruce Wayne <bruce@nessaja>
2015-04-22 13:01:50 +0200

    Added generation of normal density by unif distr. values via boy-muller.

--- exercise_01.R
+++ exercise_01.R
@@ -11,5 +11,25 @@ gen_nd <- function(n,m,rate) {
   curve(dnorm, col='red', add=TRUE)
 }
 
-n = 10000
-gen_nd(n, n, 1)
+#n = 10000
+#gen_nd(n, n, 1)
+
+gen_nd_boxmul <- function(n) {
+  y <- 1:n
+  for(i in 1:n) {
+    U <- runif(1)
+    V <- runif(1)
+  
+    X <- sqrt(-2*log(U)) * cos(2*pi*V)
+    Y <- sqrt(-2*log(U)) * sin(2*pi*V)
+    
+    y[i + 0] <- X
+    y[i + 1] <- Y
+  }
+  
+  hist(y, freq=FALSE, breaks=50)
+  curve(dnorm, col='red', add=TRUE)
+}
+
+n <- 10000
+gen_nd_boxmul(n)
