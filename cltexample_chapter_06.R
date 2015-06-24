d0f75d3f14e8164c13747a7b37089ef91cb680f9
Matthias <matthias.baumgarten@protonmail.ch>
2015-06-24 13:51:02 +0200

    Boostrap in chapter 6.

--- cltexample_chapter_06.R
+++ cltexample_chapter_06.R
@@ -20,6 +20,47 @@ clt.bounds <- function(x, y, alpha=.1) {
   plot(main="X and Y data", x, y, xlab="X data", ylab="y data")
 }
 
+bootstrap.bounds <- function(x, y, m = 1000) {
+  n        <- length(x)
+  lin.mod  <- lm(y~x-1)
+  est.beta <- as.numeric(lin.mod$coefficients[1])
+  eps      <- lin.mod$residuals
+  sn       <- my.sn(lin.mod$residuals, x, n)
+  
+  Ts  <- c(1:m)
+  for(i in Ts) {
+    # 1 resample
+    
+    ### must be modified!!!
+    eps.star <- sample(eps.cent, replace = TRUE)
+    Z        <- x * est.beta + eps.star
+    
+    # 2 calculate T's
+    lin.mod.star  <- lm(Z~x-1)
+    est.beta.star <- as.numeric(lin.mod.star$coefficients[1])
+    sn.star       <- as.numeric(my.sn(lin.mod.star$residuals, x, n))
+    
+    Ts[i]         <- sqrt(n) * (est.beta.star - est.beta)/sn.star
+  }
+  
+  # sort T's
+  Ts <- sort(Ts)
+  
+  lower.bound <- est.beta - (Ts[950]*sn/sqrt(n))
+  upper.bound <- est.beta - (Ts[50]*sn/sqrt(n))
+  
+  printf("Bootstrap - lower bound = %.4f\n", lower.bound)
+  printf("Bootstrap - upper bound = %.4f\n", upper.bound)
+}
+
+my.sn <- function(x) {
+  n               <- length(n)
+  big.eps.inverse <- 1/((x%*%x)/n)
+  big.m           <- 
+    
+  return (sqrt(big.eps.inverse * big.m * big.eps.inverse))
+}
+
 # creation of simulated observations
 n          <- 50
 beta.given <- 3
@@ -30,3 +71,4 @@ y    <- beta.given * x + eps
 
 printf("        real beta = %.4f\n", beta.given)
 clt.bounds(x, y, alpha = .1)
+bootstrap.bounds(x, y)
